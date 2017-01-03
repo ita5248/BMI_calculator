cb43eff6daa3e61d4581e1498a1cab6fc1769c3a
Mateusz <matesz.pietrzykowski@wp.pl>
2017-01-03 21:50:08 +0100

    test

--- przyklady/setup.py
+++
@@ -1,8 +0,0 @@
-import sys
-from cx_Freeze import setup, Executable
-
-setup(
-    name = "BMI",
-    version = "0.1",
-    description = "BMI.",
-    executables = [Executable("main2.py", base = "Win32GUI")])
